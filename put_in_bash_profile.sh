00674a1946c8849a88c063669f3fe4af7a73a728
alex-greff <alex.j.greff@gmail.com>
2020-06-21 20:28:00 -0400

    Added --rm option to alias commands

--- put_in_bash_profile.sh
+++ put_in_bash_profile.sh
@@ -1,3 +1,3 @@
 # Dev containers
-alias c_dev_container='docker run -v "/${PWD}":/app -it alexgreff/c_dev_container:latest bash'
-alias autograder_container='docker run -v "/${PWD}":/app -it alexgreff/autograder_container:latest bash'
+alias c_dev_container='docker run --rm -v "/${PWD}":/app -it alexgreff/c_dev_container:latest bash'
+alias autograder_container='docker run --rm -v "/${PWD}":/app -it alexgreff/autograder_container:latest bash'
