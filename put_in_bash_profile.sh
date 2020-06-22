609a3d9b17d611815e452c80f3ae211da1e8c89f
alex-greff <alex.j.greff@gmail.com>
2020-06-21 20:26:01 -0400

    Small change

--- put_in_bash_profile.sh
+++ put_in_bash_profile.sh
@@ -1,5 +1,3 @@
-# C container
+# Dev containers
 alias c_dev_container='docker run -v "/${PWD}":/app -it alexgreff/c_dev_container:latest bash'
-
-# Autograder container
 alias autograder_container='docker run -v "/${PWD}":/app -it alexgreff/autograder_container:latest bash'
